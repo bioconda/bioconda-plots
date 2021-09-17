62958a749fa910c47eb453de8461a8662c2fbf12
Felix Wiegand <fxwiegand@gmail.com>
2021-09-17 11:18:43 +0200

    First plot and update README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bioconda-plots
 Plotting bioconda package downloads over cdf of all packages
+
+## Usage
+
+`python src/plot.py | vl2png > plot.png`
--- src/plot.py
+++ src/plot.py
@@ -1,7 +1,9 @@
 import os
 import json
+import sys
 
 downloads = {}
+plot_data = []
 
 for filename in os.listdir('bioconda-stats/package-downloads/anaconda.org/bioconda/'):
     if filename.endswith(".json"):
@@ -9,4 +11,13 @@ for filename in os.listdir('bioconda-stats/package-downloads/anaconda.org/biocon
             data = json.load(file)
             downloads[filename.strip(".json")] = data["downloads_per_date"].pop()["total"]
 
-print(downloads)
+for i, (package, download_count) in enumerate(sorted(downloads.items(), key=lambda item: item[1])):
+    if i == 0:
+        plot_data.append({"pos": i, "count": download_count})
+    else:
+        plot_data.append({"pos": i, "count": plot_data[i - 1]["count"] + download_count})
+
+with open("src/plot.vl.json", "r") as vl_specs:
+    plot = json.load(vl_specs)
+    plot["data"]["values"] = plot_data
+    sys.stdout.write(json.dumps(plot))
