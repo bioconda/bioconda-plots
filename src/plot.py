8328869f5824a3e1fe14e068ee10581017fc02ee
Felix Wiegand <fxwiegand@gmail.com>
2021-09-20 16:37:36 +0200

    Add argparse for package to be plotted

--- src/plot.py
+++ src/plot.py
@@ -1,6 +1,15 @@
 import os
 import json
 import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Package to be plotted.')
+parser.add_argument('package', type=str,
+                    help='Package to be plotted over cdf of all packages')
+
+args = parser.parse_args()
+
+to_be_plotted = args.package
 
 downloads = {}
 plot_data = []
@@ -11,13 +20,18 @@ for filename in os.listdir('bioconda-stats/package-downloads/anaconda.org/biocon
             data = json.load(file)
             downloads[filename.strip(".json")] = data["downloads_per_date"].pop()["total"]
 
+max_downloads = max(downloads.values())
+data = [0] * max_downloads
+
 for i, (package, download_count) in enumerate(sorted(downloads.items(), key=lambda item: item[1])):
-    if i == 0:
-        plot_data.append({"pos": i, "count": download_count})
-    else:
-        plot_data.append({"pos": i, "count": plot_data[i - 1]["count"] + download_count})
+    for i in range(download_count, max_downloads):
+        data[i] += 1
+
+for (i, d) in enumerate(data):
+    plot_data.append({"pos": i, "count": d})
+
 
 with open("src/plot.vl.json", "r") as vl_specs:
     plot = json.load(vl_specs)
     plot["data"]["values"] = plot_data
-    sys.stdout.write(json.dumps(plot))
+    sys.stdout.write(json.dumps(plot, indent=4))
