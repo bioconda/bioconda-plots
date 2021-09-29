7a736d3870644e66a18f21c24796eae66b7c737e
Felix Wiegand <fxwiegand@gmail.com>
2021-09-29 18:09:50 +0200

    Reduce version downloads to latest 7 versions

--- src/plot_versions.py
+++ src/plot_versions.py
@@ -17,7 +17,10 @@ for filename in os.listdir('bioconda-stats/package-downloads/anaconda.org/biocon
             if version_filename.endswith(".json"):
                 versions.append(version_filename[:-5])
 
-        for version in versions:
+        if all([all(list(map(lambda x: x.isdigit(), v.split('.')))) for v in versions]):
+            versions.sort(key=lambda s: list(map(int, s.split('.'))))
+
+        for version in versions[-7:]:
             with open(f"bioconda-stats/package-downloads/anaconda.org/bioconda/{package}/{version}.json", "r") as file:
                 data = json.load(file)
                 data = data["downloads_per_date"][-15:]
