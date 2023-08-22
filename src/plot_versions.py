83826bc58909f8fbe5495e473ce2df72a59d60b5
aliciaaevans <alicia.evans@nih.gov>
2023-08-22 10:08:58 -0400

    error handling, don't pull tag for current date

--- src/plot_versions.py
+++ src/plot_versions.py
@@ -2,6 +2,11 @@ import io
 import os
 import pandas as pd
 from git import Repo
+from logging import DEBUG, basicConfig, getLogger
+
+
+basicConfig(level=DEBUG)
+logger = getLogger(__name__)
 
 if not os.path.exists("plots"):
     os.makedirs("plots")
@@ -15,52 +20,62 @@ for filename in os.listdir(
 ):
     if filename.endswith(".tsv"):
         package = filename[:-4]
+        try:
+            logger.info(f"Loading data for package: {package}")
+            package_df = pd.DataFrame()
+            tagref = None
 
-        if not os.path.exists(f"plots/{package}"):
-            os.makedirs(f"plots/{package}")
+            df = pd.read_csv(f"bioconda-stats/package-downloads/anaconda.org/bioconda/versions/{filename}", encoding="utf-8", sep="\t")
+            versions = set(df["version"])
+            prev_tagname = tags[len(tags) - 1].name
 
-        versions = set()
-        package_df = pd.DataFrame()
-        df = None
-        tagref = None
-        prev_tag = None
-        # Get tags going back 15 days
-        for days_back in range(0, 15):
-            if tagref is not None:
-                prev_tag = tagref
-            tagref = repo.tags[len(repo.tags) - 1 - days_back]
-            subtree = (
-                tagref.commit.tree / "package-downloads/anaconda.org/bioconda/versions"
-            )
+            # Get tags going back 15 days
+            for days_back in range(1, 15):
+                if tagref is not None:
+                    prev_tagname = tagref.name
+                tagref = tags[len(tags) - 1 - days_back]
+                subtree = (
+                    tagref.commit.tree / "package-downloads/anaconda.org/bioconda/versions"
+                )
 
-            # Get a previous tagged version of the package stats tsv
-            try:
-                blob = subtree / filename
-            except KeyError:
-                # does not exist
-                continue
+                # Get a previous tagged version of the package stats tsv
+                try:
+                    blob = subtree / filename
+                except KeyError:
+                    # does not exist
+                    break
 
-            new_df = pd.read_csv(
-                io.BytesIO(blob.data_stream.read()), encoding="utf-8", sep="\t"
-            )
-            # do a delta between totals of different dates
-            versions = versions | set(new_df["version"])
-            if df is not None:
+                logger.debug(f"Found data for {package} from date {tagref.name}.")
+                new_df = pd.read_csv(
+                    io.BytesIO(blob.data_stream.read()), encoding="utf-8", sep="\t"
+                )
+                # do a delta between totals of different dates
+                versions = versions | set(new_df["version"])
                 df_sub = df.set_index("version").subtract(new_df.set_index("version"), fill_value=0)
                 df_sub.rename(columns={"total": "delta"}, inplace=True)
                 df = df.merge(df_sub, on="version")
-                df["date"] = prev_tag.name
+                df["date"] = prev_tagname
                 package_df = pd.concat([package_df, df], ignore_index=True)
-            df = new_df
+                df = new_df
 
-        # Sort by version (for semantic versioning)
-        versions = list(versions)
-        if all([all(list(map(lambda x: x.isdigit(), str(v).split('.')))) for v in versions]):
-            versions.sort(key=lambda s: list(map(int, str(s).split('.'))))
-        
-        package_df['version'] = pd.Categorical(package_df['version'], ordered=True, categories=versions)
-        package_df = package_df.sort_values(by=['version','date'])[["date", "total", "delta", "version"]]
+            if len(package_df.index) > 0:
+                # Sort by version (for semantic versioning)
+                versions = list(versions)
+                if all([all(list(map(lambda x: x.isdigit(), str(v).split(".")))) for v in versions]):
+                    versions.sort(key=lambda s: list(map(int, str(s).split("."))))
+                
+                package_df["version"] = pd.Categorical(package_df["version"], ordered=True, categories=versions)
+                package_df = package_df.sort_values(by=["version","date"])[["date", "total", "delta", "version"]]
 
-        # Save plot data
-        with open(f"plots/{package}/versions.json", "w") as v:
-            v.writelines(package_df.to_json(orient='records'))
+                # Save plot data
+                if not os.path.exists(f"plots/{package}"):
+                    os.makedirs(f"plots/{package}")
+                with open(f"plots/{package}/versions.json", "w") as v:
+                    v.writelines(package_df.to_json(orient="records"))
+                    logger.debug(f"Saved data for {package} to versions.json.")
+
+        except Exception as e:
+            # Log package name and continue with the rest
+            e.args = (f"Error creating plot for {package}.",) + e.args
+            logger.exception(e)
+
